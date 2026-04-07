ef2a416fc699e1bf9f3db2fae9a5d267f641ef89
Boburbek <zboburbek@mail.ru>
2026-04-07 12:02:42 +0500

    post count title visible

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -37,7 +37,7 @@
 >
 
     {{-- TITLE --}}
-    <h1 class="text-2xl font-bold mb-6 text-white">
+    <h1 class="text-2xl font-bold mb-6 text-black">
         Posts ({{ count($this->filteredPosts) }})
     </h1>
 
